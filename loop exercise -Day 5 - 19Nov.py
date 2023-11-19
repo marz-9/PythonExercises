1df6e437f8ebd1a70333aa7065576b24c76fc220
marz-9 <150155782+marz-9@users.noreply.github.com>
2023-11-19 17:04:44 +0400

    Day 5 19 Nov

---
+++ loop exercise -Day 5 - 19Nov.py
@@ -0,0 +1,25 @@
+#Question 2 Star shape
+
+#Question 3 Perfect number
+summ = 0
+n=int(input("Enter a number:"))
+#for n in range (1, 101):
+for y in range(1,n+1):
+    for i in range (1, y):
+        if(y%i == 0):
+            summ+=i
+    if (summ == y):
+        print(str(summ)+" is a perfect number")
+    summ=0
+
+#Question 4 Narcissitic number
+number=int(input("Enter a number: "))
+
+for number in range(1,number+1):
+    result = 0
+    for digit in str(number):
+        result += (int(digit) ** 3)   
+        if (result == number):
+            print(number)
+            print("yes")
+            
