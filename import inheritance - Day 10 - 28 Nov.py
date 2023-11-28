fad0146c22ce276e09c74298838ce7ddd92b9cfe
marz-9 <150155782+marz-9@users.noreply.github.com>
2023-11-28 15:10:24 +0400

    Delete import inheritance - Day 10 - 28 Nov.py

--- import inheritance - Day 10 - 28 Nov.py
+++
@@ -1,9 +0,0 @@
-from inheritance import *
-def main():
-    person5=Student("Maryam")
-    person1= Person("Fatma",24)
-    person2= Person("Sara")
-    person1.say_hi()
-    person5.say_hi()
-
-main()
